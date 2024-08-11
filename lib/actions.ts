795679a0044570c1419b1a0c8a95764730041882
yuukun140 <yuukun140@gmail.com>
2024-08-11 16:51:40 +0900

    ログアウト処理の修正

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,7 +2,7 @@
 
 import { signIn } from './../auth';
 import { AuthError } from 'next-auth';
-
+import { signOut } from '@/auth';
 
 export async function authenticate(
     prevState: string | undefined,
@@ -22,3 +22,5 @@ export async function authenticate(
       throw error;
     }
   }
+
+export async function signOut(){}
